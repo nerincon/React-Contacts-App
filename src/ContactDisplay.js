6ad0d7011c54830aa73ef6a25202f7df0968a48d
nerincon <nerincon1@gmail.com>
2018-06-20 19:30:00 -0500

    fixed warnings

--- src/ContactDisplay.js
+++ src/ContactDisplay.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import {getContacts, deleteContact} from './contact-func.js'
 import ContactsTable from './ContactDetailsPage'
 import './ContactDisplay.css'
 
--- src/HomePage.js
+++ src/HomePage.js
@@ -5,7 +5,7 @@ function Homepage () {
   return (
     <section>
       <h2>Welcome to your contacts application!</h2>
-      <img src="/img/contact.jpg" alt="mycontacts"/>
+      <img src='/img/contact.jpg' alt='mycontacts' />
     </section>
   )
 }
